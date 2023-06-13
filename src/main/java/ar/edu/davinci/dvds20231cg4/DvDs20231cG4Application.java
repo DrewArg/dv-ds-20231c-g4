3d1341389be19964927b6adaa8ba284afbd574d8
DrewArg <andresfabbiano5@gmail.com>
2023-06-13 20:30:33 -0300

    first commit

---
+++ src/main/java/ar/edu/davinci/dvds20231cg4/DvDs20231cG4Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20231cg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20231cG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20231cG4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20231cg4/DvDs20231cG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20231cg4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20231cG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
